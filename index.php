16d3aa1b898c2f06af35e11a5c077e5311946d7c
Aisell-78 <kirzuofficial@gmail.com>
2026-01-29 15:15:37 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+//keyword keyword itu sebuah fungsi standar dalam php
+echo "<pre>";
+echo "hai dunia<br>"; //echo untuk ...
+var_dump("hai dunia"); //var_dump untuk ...
+var_dump(10000);
+var_dump(100.25);
